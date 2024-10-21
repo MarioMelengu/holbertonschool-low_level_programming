a04f18344171c6f431c7fa1232c981d30f4f0b4a
MarioMelengu <10030@holbertonstudents.com>
2024-10-21 12:04:52 +0000

    printf2

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,8 +5,8 @@
  * Return: Always 0
  */
 int main(void) {
-    printf("Size of char: %zu byte(s)\n", sizeof(char));
-    printf("Size of int: %zu byte(s)\n", sizeof(int));
+    printf("Size of char: %ld byte(s)\n", sizeof(char));
+    printf("Size of int: %ld byte(s)\n", sizeof(int));
     printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
