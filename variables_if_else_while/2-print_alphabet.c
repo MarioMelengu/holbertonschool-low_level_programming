bb1e8da372c2d0645d91b0c5223b23cf2ab4c739
MarioMelengu <10030@holbertonstudents.com>
2024-10-22 11:40:36 +0000

    putchar

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -9,13 +9,11 @@ int main(void)
 {
 	char ch;
 
-	/* Print the lowercase alphabet */
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
 
-	/* Print a new line */
 	putchar('\n');
 
 	return (0);
