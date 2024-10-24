cf7b0ed237c9019f19ca8c055006179cb39d822f
MarioMelengu <10030@holbertonstudents.com>
2024-10-24 12:26:55 +0000

    alphax10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
-void print_alphabet_x10(void);
+/*
+* print_alphabet 10 times - sxvgfg
+*/
+
+void print_alphabet_x10(void)
 {
-    char letter;
-    for(letter = 'a'; letter <= 'z'; letter++)
-    
+	int i;
+	char letter;
 
-
+	for (i = 1; i <= 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
 }
