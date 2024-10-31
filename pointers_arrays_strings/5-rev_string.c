1f678b58bdce959a2683228ed8b433ad63c97d38
MarioMelengu <10030@holbertonstudents.com>
2024-10-31 13:26:20 +0000

    new file

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -5,5 +5,21 @@
 */
 void rev_string(char *s)
 {
-	
+	int count, start, end, tmp;
+
+	count = 0;
+	while (s[count] != '\0')
+		count++;
+
+	start = 0;
+	end = count - 1;
+
+	while (start < end)
+	{
+		tmp = s[start];
+		s[start] = s[end];
+		s[end] = tmp;
+		start++;
+		end--;
+	}
 }
