f4b6d03b095e6b364dd01f2f47bf42896634cd3d
MarioMelengu <10030@holbertonstudents.com>
2024-10-31 14:53:38 +0000

    git push

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,9 +8,10 @@
 void puts_half(char *str)
 {
 int i;
-
-for (i = 0; str[i] != ('\0'); i++)
-if (i / 5)
-_putchar(str[i]);
+int j;
+for (i = 0; str[i] != '\0'; i++)
+j = (i - 1) / 2;
+for (j = (i - 1) / 2 + 1; str[j] != '\0'; j++)
+_putchar(str[j]);
 _putchar('\n');
 }
