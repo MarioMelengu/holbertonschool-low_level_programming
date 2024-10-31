559f18ecc2bdb67cb5d5dc23bd76813682ce6702
MarioMelengu <10030@holbertonstudents.com>
2024-10-31 13:13:53 +0000

    changes

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -5,22 +5,12 @@
 */
 void print_rev(char *s)
 {
-	int i;
-	int last_index = -1;
+	int i, count;
 
-	if (s[0] == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		last_index = i;
-	}
-	for (i = last_index; i >= 0; i--)
-	{
+	count = 0;
+	while (s[count] != '\0')
+		count++;
+	for (i = count - 1; i >= 0; i--)
 		_putchar(s[i]);
-	}
 	_putchar('\n');
 }
--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -5,16 +5,5 @@
 */
 void rev_string(char *s)
 {
-    int i;
-	int last_index = -1;
-
-	if (s[0] == '\0')
-	{
-	}
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		last_index = i;
-	}
-	for (i = last_index; i >= 0; i--)
+	
 }
