16fff1191d83f664b486e8bf151ead8509e28e96
MarioMelengu <10030@holbertonstudents.com>
2024-11-05 10:24:48 +0000

    added new file

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,8 +19,8 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *_memset(char *s, char b, unsigned int n);
-
-
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 
 char *_strstr(char *haystack, char *needle);
