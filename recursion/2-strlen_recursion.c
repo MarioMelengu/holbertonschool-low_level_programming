15e0f548d830b6ebfdeafb0c05caab7c2d9f23dc
MarioMelengu <10030@holbertonstudents.com>
2024-11-06 12:46:26 +0000

    added new file

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,9 @@
+#include"main.h"
+
+/**
+ * _strlen_recursion -entry point,returns length of string
+ * @s: -holds value for output
+ * Return: (1 + _strlen_recursion(s + 1))
+ */
+
+int _strlen_recursion(char *s)
