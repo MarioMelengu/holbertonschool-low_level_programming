6a2fe18f0e8d9cf3609e461576c75d263891f7e8
MarioMelengu <10030@holbertonstudents.com>
2024-11-06 13:22:51 +0000

    added new file

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -8,10 +8,12 @@
 
 int factorial(int n)
 {
-	int i;
-	
-	if(n < 0)
+	if (n < 0)
 	return (-1);
+
+	else if (n == 0)
+	return (1);
+
 	else
-	return (i * factorial(n - 1));
+	return (n * factorial(n - 1));
 }
