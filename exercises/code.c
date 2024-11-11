76ff839ae76cc6bb1170df12fc1074de65392915
MarioMelengu <10030@holbertonstudents.com>
2024-11-11 09:44:42 +0000

    exercise

--- exercises/code.c
+++ exercises/code.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 
-void calculateSumAndAverage(float arr[], int size, float *sum, float *average)
+void calculateSumAndAverage(float *arr, int size, float sum, float average)
 {
 	int i;
     for (i = 0; i < size; i++) 
-        *sum += arr[i]; 
+        sum += arr[i]; 
     
     if (size > 0) 
-		*average = *sum / size;
+		average = sum / size;
+
+	printf("Sum = %.1f\n", sum);
+    printf("Average = %.2f\n", average);
 }
+
+
 
 int main(void)
 {
     float arr[] = {5.0, 3.5, 6.0, 2.5};
     int size = sizeof(arr) / sizeof(arr[0]);
-    float sum;
-    float average;
+	float sum;
+	float average;
 
-    calculateSumAndAverage(arr, size, &sum, &average);
-
-    printf("Sum = %.1f\n", sum);
-    printf("Average = %.2f\n", average);
+    calculateSumAndAverage(arr, size, sum, average);
 
     return (0);
 }
