3970fcf1586ee1364ac5438bd8347e23c57f6789
MarioMelengu <10030@holbertonstudents.com>
2024-11-11 13:44:25 +0000

    changes

--- exercises/code.c
+++ exercises/code.c
@@ -8,22 +8,17 @@ void calculateSumAndAverage(float *arr, int size)
 	
     for (i = 0; i < size; i++) 
     	sum += arr[i]; 
-    
-    if (size > 0)
+
+	if (size > 0)
 		average = sum / size;
 
-	printf("Sum = %.1f\n", sum);
-    printf("Average = %.2f\n", average);
+	printf("Sum = %.1f  Average = %.2f\n", sum, average);
 }
 
 int main(void)
 {
     float arr[] = {5.0, 3.5, 6.0, 2.5};
-    int size = sizeof arr / sizeof arr[0];
-	float sum;
-	float average;
-
-    calculateSumAndAverage(arr, size);
-
+    int size = sizeof(arr) / sizeof(arr)[0];
+	calculateSumAndAverage(arr, size);
     return (0);
 }
