cd48b8f12eb10c3c1b6d6fd1d58ea693cf8e7100
MarioMelengu <10030@holbertonstudents.com>
2024-11-13 13:08:31 +0000

    added new file

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
+#include <stdio.h>
 #include "dog.h"
-#include <stddef.h>
 
 /**
  * init_dog -entry point,initializes a variable
@@ -13,8 +13,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 	}
+
 }
