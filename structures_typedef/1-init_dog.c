bc9838868e55f1fd2541832f3d050555e60929e3
MarioMelengu <10030@holbertonstudents.com>
2024-11-13 23:21:16 +0000

    last time

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include "dog.h"
-
 /**
- * init_dog - Entry point, initializes a variable.
- * @d: points to specific memory.
- * @name: holds value for output.
- * @age: holds value for output.
- * @owner: holds value for output.
+ * init_dog -entry point,initializes a variable
+ * @d: -points to specific memory
+ * @name: -holds value for output
+ * @age: -holds value for output
+ * @owner: -holds value for output
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d != NULL)
-{
-d->name = name;
-d->age = age;
-d->owner = owner;
-}
+	if (d != NULL)
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+
 }
