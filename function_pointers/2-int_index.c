f88f90982df25efff667b3892e785642e979bf5d
MarioMelengu <10030@holbertonstudents.com>
2024-11-19 12:32:11 +0000

    new file

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * int_index -entry point,searches for an int
+ * @array: -points to the respective array
+ * @size: -holds value for output
+ * @cmp: -points to the respective func
+ * Return: (i)
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+
+	i = 0;
+	while (i < size)
+	{
+		if (cmp(*(array + i)))
+			break;
+		i++;
+	}
+		if (i == size)
+			return (-1);
+
+	return (i);
+}
